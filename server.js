5462b4bea06e3197cfcfea3b1aad6f2b3cecefc6
Aaron Diggdon <aarondiggdon@gmail.com>
2020-10-29 16:53:01 -0700

    Added box and color scheme

---
+++ server.js
@@ -0,0 +1,46 @@
+var mysql = require("mysql");
+var connection;
+var express = require("express");
+
+const axios = require("axios");
+axios
+    .get("https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&query=flat%20earth&key=AIzaSyAYJ05r2WOK34MO9zLkmaz0Ux9NWnYTCcI")
+    .then(function(res) {
+        console.log(res.data);
+    });
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+// Requiring our models for syncing
+var db = require("./models");
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Static directory
+app.use(express.static("public"));
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+// require("./routes/autho-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "factcheck_db"
+    });
+}
