63bfd965c241472b2fe6c759296acb4ff475915d
Aaron Diggdon <aarondiggdon@gmail.com>
2020-10-30 15:10:37 -0700

    Added search button and ripple effects

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,42 @@ $(document).ready(function() {
         }
     });
 
+    $("#search").click(function(e) {
 
+        // Remove any old one
+        $(".search").click(function(e) {
+                $(".ripple").remove();
+            })
+            // Setup
+        var posX = $(this).offset().left,
+            posY = $(this).offset().top,
+            buttonWidth = $(this).width(),
+            buttonHeight = $(this).height();
+
+        // Add the element
+        $(".search").append("<span class='ripple'></span>");
+
+        console.log($(this));
+        // Make it round!
+        if (buttonWidth >= buttonHeight) {
+            buttonHeight = buttonWidth;
+        } else {
+            buttonWidth = buttonHeight;
+        }
+
+        // Get the center of the element
+        var x = e.pageX - posX - buttonWidth / 2;
+        var y = e.pageY - posY - buttonHeight / 2;
+
+
+        // Add the ripples CSS and start the animation
+        $(".ripple").css({
+            width: buttonWidth,
+            height: buttonHeight,
+            top: y + 'px',
+            left: x + 'px'
+        }).addClass("rippleEffect");
+    });
 
 
 
--- server.js
+++ server.js
@@ -1,32 +1,45 @@
-// Requiring necessary npm packages
+var mysql = require("mysql");
+var connection;
 var express = require("express");
-var session = require("express-session");
-// Requiring passport as we've configured it
-var passport = require("./config/passport");
 
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
+const axios = require("axios");
+axios
+    .get("https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&query=flat%20earth&key=AIzaSyAYJ05r2WOK34MO9zLkmaz0Ux9NWnYTCcI")
+    .then(function(res) {
+        console.log(res.data);
+    });
 
-// Creating express app and configuring middleware needed for authentication
+// Sets up the Express App
+// =============================================================
 var app = express();
-
+var PORT = process.env.PORT || 8080;
+// Requiring our models for syncing
+var db = require("./models");
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Static directory
 app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Requiring our routes
-require("./routes/html-routes.js")(app);
+// Routes
+// =============================================================
 require("./routes/api-routes.js")(app);
-
-// Syncing our database and logging a message to the user upon success
-// listens to the PORT
-db.sequelize.sync().then(function() {
+// require("./routes/autho-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
-        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+        console.log("App listening on PORT " + PORT);
     });
 });
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "factcheck_db"
+    });
+}
