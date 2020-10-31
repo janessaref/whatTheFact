a1bd8f4bd629dfbe658bed25637d6ba2522ea4c6
Aaron Diggdon <aarondiggdon@gmail.com>
2020-10-31 12:18:10 -0700

    Added frontend buttons and animations

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
 var mysql = require("mysql");
 var connection;
 var express = require("express");
+var session = require("express-session");
+var passport = require("./config/passport");
+// const axios = require("axios");
+// axios
+//     .get("https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&query=flat%20earth&key=AIzaSyAYJ05r2WOK34MO9zLkmaz0Ux9NWnYTCcI")
+//     .then(function(res) {
+//         console.log(res.data);
+//     });
 
-const axios = require("axios");
-axios
-    .get("https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&query=flat%20earth&key=AIzaSyAYJ05r2WOK34MO9zLkmaz0Ux9NWnYTCcI")
-    .then(function(res) {
-        console.log(res.data);
-    });
+
+
+//Rachel's server.js
+
+// // Requiring necessary npm packages
+// var express = require("express");
+
+// // Requiring passport as we've configured it
+
+
+// // Setting up port and requiring models for syncing
+// var PORT = process.env.PORT || 8080;
+// var db = require("./models");
+
+// // Creating express app and configuring middleware needed for authentication
+// var app = express();
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static("public"));
+// // We need to use sessions to keep track of our user's login status
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// // Requiring our routes
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+// // Syncing our database and logging a message to the user upon success
+// // listens to the PORT
+// db.sequelize.sync().then(function() {
+//     app.listen(PORT, function() {
+
 
 // Sets up the Express App
 // =============================================================
@@ -29,7 +65,7 @@ require("./routes/html-routes.js")(app);
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
+        console.log("App listening on https://localhost:" + PORT);
     });
 });
 
@@ -39,7 +75,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "root",
+        password: "password",
         database: "factcheck_db"
     });
 }
