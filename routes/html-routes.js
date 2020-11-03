02bd7f1c194c8bd014c78a7ca0f384b6cd469e39
Game Over <janessarefong@gmail.com>
2020-11-02 17:16:04 -0800

    add comments and polish code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,12 @@
+// require path and our authentication.js file for users
 var path = require("path");
 var authentication = require("../config/middleware/authentication");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
 
-    // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to
 
-    // index route loads view.html
-    //   app.get("/", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/main.html"));
-    //   });
-
-    // index route loads cms.html
+    // index route loads index.html
     app.get("/", function(req, res) {
         if (req.user) {
             res.redirect("/user");
@@ -20,28 +14,26 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    // user route loads blog.html
-    // app.get("/user", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/user.html"));
-    // });
-
+    // about route loads about.html
     app.get("/about", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/about.html"));
     });
+    // contact route loads contact.html
     app.get("/contact", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/contact.html"));
     });
 
-
-    // signup route loads author-manager.html
+    // signup route loads signup.html
     app.get("/signup", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
 
+    // profile route loads profile.html
     app.get("/profile", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/profile.html"));
     });
 
+    // login route loads login.html and redirects users to the user route
     app.get("/login", function(req, res) {
         if (req.user) {
             res.redirect("/user");
@@ -49,9 +41,8 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
+    // user route loads user.html but the user has to be authenticated first before the next function can be executed, otherwise they would be directed to the index.html
     app.get("/user", authentication, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/user.html"));
     });
-
-
 };
