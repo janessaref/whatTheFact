9500575351620a8414e35f900a09cce75fda4f8f
Game Over <janessarefong@gmail.com>
2020-11-02 17:21:10 -0800

    add comments and clean up code in api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,30 +1,28 @@
-// const axios = require("axios");
-//  app.get put inside of a route
+// require our models and passport.js file
 var passport = require("../config/passport");
 var db = require("../models");
 
 // Routes
-// =============================================================
 module.exports = function(app) {
-
+    // this route grabs the data of a specific user
     app.get("/api/user/:id", function(req, res) {
         db.User.findOne({
             where: {
                 id: req.params.id
             }
         }).then(function(dbUser) {
-            res.json(dbUser)
+            res.json(dbUser);
         });
     });
 
-    // If the user has valid login credentials, send them to the members page.
+    // If the user has valid login credentials, send them to the user page
     // Otherwise the user will be sent an error
     app.post("/api/login", passport.authenticate("local"), function(req, res) {
         res.json(req.user);
     });
 
+    // when the user signs up, save their credentials into the user database
     app.post("/api/signup", function(req, res) {
-        // console.log(req.body)
         db.User.create({
                 // firstname: req.body.firstname,
                 // lastname: req.body.lastname,
@@ -32,7 +30,7 @@ module.exports = function(app) {
                 email: req.body.email,
                 password: req.body.password
             })
-            //after that redirect to the login 
+            // after that redirect to the login 
             .then(function() {
                 res.redirect(307, "/api/login");
             }, function(err) {
@@ -41,29 +39,25 @@ module.exports = function(app) {
             });
     });
 
-    // Route for logging user out
+    // Route for logging the user out
     app.get("/logout", function(req, res) {
         req.logout();
         res.redirect("/");
     });
 
-
-    //Returning JSON data for all searches for a specific user -FROM THE API
-
+    // grabs the info of a user who is currently logged in
     app.get("/api/user_data", function(req, res) {
         if (!req.user) {
             // The user is not logged in, send back an empty object
             res.json({});
         } else {
             // Otherwise send back the user's email and id
-            // Sending back a password, even a hashed password, isn't a good idea
             res.json({
                 username: req.user.username,
                 email: req.user.email,
                 id: req.user.id
             });
-        }
+        };
     });
-
     //END OF MODULES, DELETE AND SOMEONE MIGHT CRY!!!
 }
